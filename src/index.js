ad3bf20daca43a66c7b5517dc0c907e3289efe35
cColds <103373668+cColds@users.noreply.github.com>
2023-01-20 13:40:37 -0600

    Fix correct/incorrect icon not centering

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const email = document.querySelector("#email");
+const country = document.querySelector("#country");
+const zipcode = document.querySelector("#zipcode");
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirm-password");
+const signUpButton = document.querySelector(".sign-up-button");
+
+// add event listeners to inputs
+
+// email.addEventListener()
+country;
+zipcode;
+password;
+confirmPassword;
+signUpButton;
+
+// when typed, check validity, and display error or correct
+
+// password will check if matched
+
+//sign up button disabled unless all are valid
